6886dcc4f60a0a6dd8447d8149d550dd52501314
Timothy W. Hilton <thilton@ucmerced.edu>
2015-01-16 16:08:58 -0800

    diagnostic LRU map, looping through GPP models
    - plot a diagnostic map of the LRU values derived from the C4 vegetation
     percentage (Still et al, 2008) and the C3/C4 LRU values of Stimler et
     al (2011)
    - run driver now loops through all of the GPP models we have and
     calculates fCOS for each.

--- calculate_fCOS/calc_fCOS_C4pct_run_driver.py
+++ calculate_fCOS/calc_fCOS_C4pct_run_driver.py
@@ -16,46 +16,69 @@ Timothy W. Hilton, UC Merced, 13 January 2015
 import os, os.path, sys
 import subprocess
 import re
-from stem_pytools import ecampbell300_data_paths as edp
 import netCDF4
+import matplotlib.pyplot as plt
+from stem_pytools import ecampbell300_data_paths as edp
+from stem_pytools import na_map
+from stem_pytools import STEM_parsers as sp
+from timutils import colormap_nlevs
 
-print('python: {}'.format(sys.executable))
 
-# get the GPP I/O API file for the CASA m15 file
-runs = edp.get_runs()
-this_run = runs['casa_m15_161']
-fname_GPP = this_run.gpp_path
+
+#============================================================
+def fCOS_from_C4pct_diagnostics():
+
+    draw_LRU_map()
+
+#============================================================
+def draw_LRU_map():
+    nc = netCDF4.Dataset(os.environ['LRU_FILE'])
+    LRU = nc.variables['LRU'][...].squeeze()
+    nc.close()
+    stem_lon, stem_lat, topo = sp.parse_STEM_coordinates(
+        os.path.join(os.getenv('SARIKA_INPUT'), 'TOPO-124x124.nc'))
+    fcos_cmap, fcos_norm = colormap_nlevs.setup_colormap(
+        LRU.min(),
+        LRU.max(),
+        nlevs=10,
+        cmap=plt.get_cmap('Blues'))
+    lru_map = na_map.NAMapFigure(cb_axis=True,t_str='LRU from C4 veg pct')
+    cm = lru_map.map.pcolor(stem_lon, stem_lat, LRU,
+                            cmap=fcos_cmap,
+                            latlon=True, 
+                            norm=fcos_norm)
+    plt.colorbar(cm, cax=lru_map.ax_cmap)
+    lru_map.fig.savefig('LRU_from_c4pct.png')
+
+run_diagnostics = True
+
+# compile the fortran code
+subprocess.call('make -f calc_fCOS_C4pct.mk clobber', shell=True)
+subprocess.call('make -f calc_fCOS_C4pct.mk', shell=True)
+
 fname_C4pct = os.path.join('/home', 'thilton', 'projects', 'COS (ecampbell3)', 
                          'C4_percentage', 'ISLSCP_C4_1DEG_932_regridded',
                          'C4_pct_124x124.nc')
-print('GPP file: {}'.format(fname_GPP))
-print('C4 file: {}'.format(fname_C4pct))
+# get the GPP I/O API file for the CASA m15 file
+runs = edp.get_runs()
+for this_run in runs.values()[0:1]:
+    fname_GPP = this_run.gpp_path
+    print('GPP file: {}'.format(fname_GPP))
+    print('C4 file: {}'.format(fname_C4pct))
 
-# if the GPP file exists, use I/O API fortran library to open it,
-# print out some information about the variables, and create a new
-# file with GPP replaced with a dummy variable
+    if os.path.exists(fname_GPP) and os.path.exists(fname_C4pct):
+        os.environ['GPP_INPUT'] = fname_GPP
+        os.environ['C4pct_INPUT'] = fname_C4pct
+        os.environ['RATIO_FILE'] = './COS_CO2_ratio_const_1.1.nc'
+        os.environ['LRU_FILE'] = './LRU_from_C4pct.nc'
+        os.environ['fCOS_FILE'] = './fCOS_from_{}_C4pct.nc'.format(
+            re.sub('[\ \-]', '', this_run.model))
+        # run the fortran part
+        subprocess.call(
+            './calc_fCOS_C4pct.x {}'.format(re.sub(' ', '\ ', this_run.model)),
+            shell=True)
+    else:
+        print('GPP file not found')
 
-if os.path.exists(fname_GPP) and os.path.exists(fname_C4pct):
-    os.environ['GPP_INPUT'] = fname_GPP
-    os.environ['C4pct_INPUT'] = fname_C4pct
-    os.environ['RATIO_FILE'] = './COS_CO2_ratio_const_1.1.nc'
-    os.environ['LRU_FILE'] = './LRU_from_C4pct.nc'
-    os.environ['fCOS_FILE'] = './fCOS_from_{}_C4pct.nc'.format(
-        re.sub('[\ \-]', '', this_run.model))
-    # compile and run the fortran part
-    subprocess.call('make -f calc_fCOS_C4pct.mk clobber', shell=True)
-    subprocess.call('make -f calc_fCOS_C4pct.mk', shell=True)
-    subprocess.call('./calc_fCOS_C4pct.x {}'.format(re.sub(' ', '\ ', this_run.model)), 
-                    shell=True)
-
-    nc = netCDF4.Dataset(os.environ['LRU_FILE'])
-    lru = nc.variables['LRU'][...].squeeze()
-    nc.close()
-    nc = netCDF4.Dataset(os.environ['C4pct_INPUT'])
-    c4pct = nc.variables['C4pct'][...].squeeze()
-    nc.close()
-
-else:
-    print('GPP file not found')
-
-
+if run_diagnostics:
+    fCOS_from_C4pct_diagnostics()
