4bed81edd8b622ec46678545ca29ef6f4ae6bfcc
Timothy W. Hilton <thilton@ucmerced.edu>
2015-01-27 13:10:23 -0800

    add custom panel function for overall loess line

--- calculate_fCOS/get_C3C4_light_LRU_eq.R
+++ calculate_fCOS/get_C3C4_light_LRU_eq.R
@@ -24,14 +24,20 @@ mytheme[['superpose.symbol']][['pch']] <- c(15,16,17,3,4)
 mytheme[['superpose.symbol']][['col']] <- pal
 
 xy <- xyplot(LRU~PAR|C3C4, data=light_data, groups=species,
-             panel=panel.superpose,
+             panel=function(x,y,...) {
+                 ## custom panel function to add an overall loess line
+                 panel.superpose(x,y,...)
+                 panel.loess(x,y,col="darkblue",...)
+             },
              auto.key=TRUE,
              col.line=pal,
              par.settings=mytheme,
              panel.groups=function(x, y, ...){
                  panel.xyplot(x, y, ...)
                  panel.loess(x, y, ...)})
+## pdf(file='~/stimler_PAR_LRU.pdf')
 print(xy)
+## dev.off()
 
 C3_lo <- loess(LRU~PAR, data=light_data[light_data[['C3C4']] == 'C3', ])
 C4_lo <- loess(LRU~PAR, data=light_data[light_data[['C3C4']] == 'C4', ])
