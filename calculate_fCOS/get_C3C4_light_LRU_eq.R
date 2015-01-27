6ad8d877bca719b61532e44b36c1f6f0b2a5c885
Timothy W. Hilton <thilton@ucmerced.edu>
2015-01-27 09:56:34 -0800

    plot loess lines in correct colors

--- calculate_fCOS/get_C3C4_light_LRU_eq.R
+++ calculate_fCOS/get_C3C4_light_LRU_eq.R
@@ -16,16 +16,22 @@ light_data[['C3C4']][isC4] <- 'C4'
 light_data[['C3C4']][!isC4] <- 'C3'
 light_data[['C3C4']] <- as.factor(light_data[['C3C4']])
 
+
+pal <- brewer.pal(length(levels(light_data[['species']])), 'Dark2')
 mytheme <- standard.theme("pdf")
 mytheme[['superpose.symbol']][['pch']] <- c(15,16,17,3,4)
-mytheme[['superpose.symbol']][['col']] <-
-    brewer.pal(length(levels(light_data[['species']])), 'Dark2')
-xyplot(LRU~PAR|C3C4, data=light_data, groups=species,
-       auto.key = TRUE,
-       par.settings = mytheme)
+##mytheme[['superpose.line']][['col'] <- pal # doesn't affect the loess lines
+mytheme[['superpose.symbol']][['col']] <- pal
 
-xyplot(LRU~PAR|C3C4, data=light_data, groups=species, type='smooth')
-
+xy <- xyplot(LRU~PAR|C3C4, data=light_data, groups=species,
+             panel=panel.superpose,
+             auto.key=TRUE,
+             col.line=pal,
+             par.settings=mytheme,
+             panel.groups=function(x, y, ...){
+                 panel.xyplot(x, y, ...)
+                 panel.loess(x, y, ...)})
+print(xy)
 
 C3_lo <- loess(LRU~PAR, data=light_data[light_data[['C3C4']] == 'C3', ])
 C4_lo <- loess(LRU~PAR, data=light_data[light_data[['C3C4']] == 'C4', ])
