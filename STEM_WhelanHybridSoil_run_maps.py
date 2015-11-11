456666651f54e422e18da24efc53e3c7aebb0600
Timothy W. Hilton <thilton@ucmerced.edu>
2015-11-11 15:19:48 -0800

    add boundary layer, free troposphere means to Fsoil map
    - I actually meant to put that in the GEOS-Chem boundary mapper.
      D'oh!
    - next step: put the averager, AQOUT parser in a function, make them
      portable

--- STEM_WhelanHybridSoil_run_maps.py
+++ STEM_WhelanHybridSoil_run_maps.py
@@ -3,6 +3,7 @@ matplotlib.use('AGG')
 import matplotlib.pyplot as plt
 
 import numpy as np
+import numpy.ma as ma
 import os
 import os.path
 from datetime import datetime
@@ -13,6 +14,7 @@ from stem_pytools import calc_drawdown
 from stem_pytools import NERSC_data_paths as ndp
 from stem_pytools import aqout_postprocess as aqp
 from stem_pytools import STEM_mapper
+from stem_pytools import noaa_ocs
 
 # I put the Whelan-Kettle hybrid soil fluxes through STEM in pmol m-2
 # s-1 when it was expecting mol m-2 s-1.  So the AQOUT concentrations
@@ -25,6 +27,8 @@ if True:
     aqc = aqp.aqout_container(hybrid_Fsoil_run.aqout_path)
     aqc.parse(t0=datetime(2008, 7, 1), t1=datetime(2008, 8, 31, 23, 59, 59))
 
+
+    import pdb; pdb.set_trace()
     aqc.data[0] = aqc.data[0] * mol_pmol_correction
     aqc.sum()
     aqc.calc_stats()
@@ -35,36 +39,58 @@ if True:
     dd = calc_drawdown.calc_STEM_COS_drawdown(aqc.cos_mean)
 
 print('drawing map')
-norm = midpt_norm.MidpointNormalize(midpoint=0.0)
-f_or_p = 'pretty'
+cmap, norm = midpt_norm.get_discrete_midpt_cmap_norm(vmin=-40, vmax=20,
+                                                     midpoint=0.0,
+                                                     bands_above_mdpt=3,
+                                                     bands_below_mdpt=7)
+f_or_p = 'fast'
 mcm3_2_pptv = 1e12  # factor to molecules cm-3 to parts per trillion by volume
 STEM_mapper.Mapper124x124(np.mean(dd[:], axis=0).squeeze()).draw_map(
     fast_or_pretty=f_or_p,
-    t_str='1 Jul - 31 Aug mean STEM drawdown, Whelan-Kettle "hybrid" Fsoil',
-    cmap=plt.get_cmap('PuOr'),
+    t_str=('1 Jul - 31 Aug 2008 mean STEM drawdown, '
+           'Whelan-Kettle "hybrid" Fsoil'),
+    cmap=cmap,
     norm=norm)
 plt.gcf().savefig(os.path.join(os.getenv('HOME'), 'plots', 'dd_map_Fsoil.png'))
 
-# cos_t_mean = np.mean(aqc.cos_mean[8:, ...], axis=0).squeeze() * mcm3_2_pptv
 
-# cmap, norm = colormap_nlevs.setup_colormap(
-#     nlevs=9,
-#     vmin=cos_t_mean.min(),
-#     vmax=cos_t_mean.max(),
-#     cmap=plt.get_cmap('Blues'))
+[agl, asl] = noaa_ocs.get_STEMZ_height(
+    topo_fname=os.path.join(os.getenv('SARIKA_INPUT'), 'TOPO-124x124.nc'),
+    wrfheight_fname=os.path.join(os.getenv('SARIKA_INPUT'),
+                                 'wrfheight-124x124-2008-2009-22levs.nc'))
+t_steps_per_day = 4
+jul1 = (2008183 - 2008060) * t_steps_per_day
+aug31 = jul1 + (62 * t_steps_per_day)
+agl_mean = np.mean(agl[jul1:aug31, ...], axis=0)
+agl_mean = np.broadcast_arrays(agl_mean, aqc.cos_mean[8:, ...])[0]
 
-# STEM_mapper.Mapper124x124(cos_t_mean[0, ...].squeeze()).draw_map(
-#     fast_or_pretty=f_or_p,
-#     t_str='8 Jul - 31 Aug STEM sfc [COS]',
-#     cmap=cmap,
-#     norm=norm)
-# plt.gcf().savefig(os.path.join(os.getenv('HOME'), 'plots', 'cos_sfc.png'))
+free_trop = ma.masked_where(agl_mean >= 4000, aqc.cos_mean[8:, ...])
+free_trop_mean = mcm3_2_pptv * np.mean(np.mean(free_trop,
+                                               axis=0),
+                                       axis=0).squeeze()
 
-# STEM_mapper.Mapper124x124(cos_t_mean[-1, ...].squeeze()).draw_map(
-#     fast_or_pretty=f_or_p,
-#     t_str='8 Jul - 31 Aug STEM TOA [COS]',
-#     cmap=cmap,
-#     norm=norm)
-# plt.gcf().savefig(os.path.join(os.getenv('HOME'), 'plots', 'cos_TOA.png'))
+abl = ma.masked_where(agl_mean <= 2000, aqc.cos_mean[8:, ...])
+abl_mean = mcm3_2_pptv * np.mean(np.mean(abl, axis=0), axis=0).squeeze()
 
-# plt.close('all')
+cmap, norm = colormap_nlevs.setup_colormap(
+    nlevs=9,
+    vmin=np.dstack((abl_mean, free_trop_mean)).min(),
+    vmax=np.dstack((abl_mean, free_trop_mean)).max(),
+    cmap=plt.get_cmap('Blues'))
+
+STEM_mapper.Mapper124x124(abl_mean.squeeze()).draw_map(
+    fast_or_pretty=f_or_p,
+    t_str='8 Jul - 31 Aug STEM [COS] <= 2000 m; GEOS-Chem boundaries',
+    cmap=cmap,
+    norm=norm)
+plt.gcf().savefig(os.path.join(os.getenv('HOME'), 'plots', 'cos_ABL.png'))
+
+STEM_mapper.Mapper124x124(free_trop_mean.squeeze()).draw_map(
+    fast_or_pretty=f_or_p,
+    t_str='8 Jul - 31 Aug STEM [COS] >= 4000 m; GEOS-Chem boundaries',
+    cmap=cmap,
+    norm=norm)
+plt.gcf().savefig(os.path.join(os.getenv('HOME'),
+                               'plots', 'cos_free_trop.png'))
+
+plt.close('all')
