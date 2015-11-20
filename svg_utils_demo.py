3c99d951aa531e6d2ecbbe6bbbad33a3a0bee598
Timothy W. Hilton <thilton@ucmerced.edu>
2015-11-19 21:11:05 -0800

    attempt to add text to svgutils.layout, system call to inkscape
    - inkscape call is mac-specific

--- svg_utils_demo.py
+++ svg_utils_demo.py
@@ -1,9 +1,9 @@
-
+import subprocess
 import matplotlib
 matplotlib.use('AGG')
 
 
-from svgutils.transform import from_mpl
+import svgutils.transform as sg
 from svgutils.templates import VerticalLayout
 import matplotlib.pyplot as plt
 import numpy as np
@@ -17,15 +17,20 @@ try:
         plt.plot(np.random.random(100))
 
     layout = VerticalLayout()
-    sz = map(int, from_mpl(figs[0]).get_size())
+    sz = map(int, sg.from_mpl(figs[0]).get_size())
     sz[1] *= 3
     sz = map(str, sz)
     layout.set_size(sz)
-    layout.add_figure(from_mpl(figs[0]))
-    layout.add_figure(from_mpl(figs[1]))
+    layout.add_figure(sg.from_mpl(figs[0]))
+    layout.add_figure(sg.from_mpl(figs[1]))
 
-    print from_mpl(figs[0]).get_size()
-    layout.save(os.path.join(os.getenv('HOME'), 'plots', 'stack_plots.svg'))
-
+    txt1 = sg.TextElement(50, 50, "HELLO", size=12)
+    layout.append([txt1])
+    layout.save(os.path.join('/', 'tmp', 'stack_plots.svg'))
+    try:
+        print('converting to pdf')
+        subprocess.call('/Applications/Inkscape.app/Contents/Resources/bin/inkscape --export-pdf=/tmp/stack_plots.pdf /tmp/stack_plots.svg', shell=True)
+    except:
+        print('unable to run inkscape')
 finally:
     plt.close('all')
