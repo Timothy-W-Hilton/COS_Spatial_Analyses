7182deb6a72fbc68fbb38479dece2a0f795fdf89
Timothy W. Hilton <thilton@ucmerced.edu>
2016-02-11 09:50:21 -0800

    error_bar_framework now writes combined netCDF file from individual STEM runs

--- error_bar_framework.py
+++ error_bar_framework.py
@@ -6,10 +6,17 @@ from stem_pytools import aqout_postprocess as aqpp
 
 runs = ndp.get_Spatial_Paper_runs()
 aqcs = {k: aqpp.aqout_container(v.aqout_path) for k, v in runs.items()}
+fnames = []
 for k, this_aqc in aqcs.items()[0:3]:
     this_aqc.parse(t0=None, t1=None, verbose=True)
     this_aqc.sum()
     this_aqc.calc_stats()
-    fname = os.path.join(os.getenv('SCRATCH'), 'aqout_{}.nc'.format(k))
-    print "writing {}".format(fname)
-    this_aqc.stats_to_netcdf(fname)
+    fnames.append(os.path.join(os.getenv('SCRATCH'), 'AQOUT_stats',
+                               'aqout_{}.nc'.format(k)))
+    print "writing {}".format(fnames[-1])
+    this_aqc.stats_to_netcdf(fnames[-1])
+
+aqpp.combine_aqout_netcdf_files(fnames,
+                                fname_out=os.path.join(os.getenv('SCRATCH'),
+                                                       'AQOUT_stats',
+                                                       'AQ_combined.nc'))
