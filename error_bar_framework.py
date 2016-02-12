f2aae526524723a9889ac77c79dc3c5f38989332
Timothy W. Hilton <thilton@ucmerced.edu>
2016-02-12 08:40:20 -0800

    subset full spatial runs dict in error_bar_framework

--- error_bar_framework.py
+++ error_bar_framework.py
@@ -7,7 +7,9 @@ from stem_pytools import aqout_postprocess as aqpp
 runs = ndp.get_Spatial_Paper_runs()
 aqcs = {k: aqpp.aqout_container(v.aqout_path) for k, v in runs.items()}
 fnames = []
-for k, this_aqc in aqcs.items()[0:3]:
+# for k, this_aqc in aqcs.items()[(1, -1)]:
+subset = dict((k, aqcs[k]) for k in ['casa_gfed_161', 'casa_m15_C4pctLRU'])
+for k, this_aqc in subset.items():
     this_aqc.parse(t0=None, t1=None, verbose=True)
     this_aqc.sum()
     this_aqc.calc_stats()
