229c051762b29f8dda593f91da0b39c1410b64e2
Timothy W. Hilton <thilton@ucmerced.edu>
2016-02-15 13:49:30 -0800

    add standard error maps for CASA-GFED3, 1.61 to error bar framework

--- error_bar_framework.py
+++ error_bar_framework.py
@@ -1,5 +1,9 @@
+import matplotlib
+matplotlib.use('AGG')
+
 from stem_pytools import NERSC_data_paths as ndp
 from aqout_postprocess_spatial_paper import AqoutContainerSpatialPaper
+from stem_pytools import STEM_mapper
 
 runs = ndp.get_Spatial_Paper_runs()
 aqcs = {k: AqoutContainerSpatialPaper(v.aqout_path) for k, v in runs.items()}
@@ -14,3 +18,13 @@ for k, this_aqc in subset.items():
     # this_aqc.calc_stats()
     this_aqc.calc_JA_midday_drawdown()
     this_aqc.calc_JA_midday_drawdown_stderr()
+
+se_map = STEM_mapper.Mapper124x124(subset['casa_gfed_161'].dd_se)
+se_map.draw_map(t_str=('casa-GFED3 LRU=1.61 [COS] drawdown '
+                       'Jul/Aug mean standard error'))
+se_map.map.fig.savefig('./se_map.png')
+
+se_map = STEM_mapper.Mapper124x124(subset['casa_gfed_161'].dd_se_neff)
+se_map.draw_map(t_str=('casa-GFED3 LRU=1.61 [COS] drawdown '
+                       'Jul/Aug mean standard error, Neff'))
+se_map.map.fig.savefig('./se_neff_map.png')
