ceb570781dbd1b7eddc80cf0cc4558a36a51652d
Timothy W. Hilton <thilton@ucmerced.edu>
2016-02-22 12:45:14 -0700

    implement normalized drawdown confidence interval calculator

--- gradient_plot_error_bars.R
+++ gradient_plot_error_bars.R
@@ -2,7 +2,34 @@ library(plotrix)
 library(RColorBrewer)
 library(tidyr)  # could also use reshape2
 
+row_normalizer <- function(dd, norm_site='NHA') {
+    norm_data <- dd[norm_site, ]
+    for (i in seq(1, nrow(dd))) {
+        dd[i, ] <- dd[i, ] / norm_data
+    }
+    return(dd)
+}
+
+ci_normalizer <- function(dd, ci, norm_site='NHA') {
+    dd_hi <- dd + ci
+    dd_lo <- dd - ci
+
+    ddnorm <- row_normalizer(dd, norm_site)
+    ddnorm_hi <- row_normalizer(dd_hi, norm_site)
+    ddnorm_lo <- row_normalizer(dd_lo, norm_site)
+
+    ci_hi <- ddnorm_hi - ddnorm
+    ci_lo <- ddnorm - ddnorm_lo
+
+    return(list(dd=ddnorm, ci_hi=ci_hi, ci_lo=ci_lo))
+}
+
+
 dummy_data <- function() {
+    ## return a dummy set of random "observations" and "confidence
+    ## intervals" with the same row and column labels that the real
+    ## data will have.  Useful for testing the plotting code and
+    ## whether plotrix is able to produce the plot I want.
     models <- c('CASA-GFED3', 'Can-IBIS', 'SiB mech')
     site_names <- c('NHA', 'CMA', 'SCA', 'WBI', 'THD')
     marker_sequence <- c('o', 'x', 5, '+', '*')
@@ -62,34 +89,38 @@ gradient_CI_plot <- function(df, dd_col='dd', se_col='dd_se_neff') {
     pal <- brewer.pal(n_models, 'Dark2')
     marker_sequence <- c(0, 1, 3, 4, 5)
 
+    ## dfw_dd: "data frame, wide; drawdown"
     dfw_dd <- spread(df[, c('model', 'site_code', 'dd')],
                      model, dd)
     dfw_dd <- dfw_dd[, !names(dfw_dd) %in% "site_code"]
+    row.names(dfw_dd) <- site_names
     dfw_se <- spread(df[, c('model', 'site_code', 'dd_se_neff')],
                      model, dd_se_neff)
     dfw_se <- dfw_se[, !names(dfw_se) %in% "site_code"]
-
-    dfw_se <- dfw_se * 1.96 ## 95% confidence interval
+    row.names(dfw_se) <- site_names
+    ## dfw_dd: "data frame, wide; confidence interval"
+    dfw_ci <- dfw_se * 1.96 ## 95% confidence interval
 
     plotCI(1:n_sites,
            dfw_dd[[1]],
-           dfw_se[[1]],
+           dfw_ci[[1]],
            xaxt='n',
-           main="drawdown with error bars",
+           main="drawdown with 95% confidence intervals",
            ylab='Drawdown  (pptv)',
            xlab='site',
            col=pal[[1]],
-           ylim=range(cbind(dfw_dd + dfw_se, dfw_dd - dfw_se)),
+           ylim=range(cbind(dfw_dd + dfw_ci, dfw_dd - dfw_ci)),
            xlim=c(1, n_sites + 1),
            pch=marker_sequence[[1]])
     ## points(x, y, col=pal[[1]], type='l')
     axis(1, at=1:n_sites, labels=site_names)
 
-    x_offset <- seq(from=0.0, by=0.1, length.out=n_models)
-    mapply(CI_plotter, dfw_dd, dfw_se,
+    x_offset <- seq(from=0.0, by=0.05, length.out=n_models)
+    mapply(CI_plotter, dfw_dd, dfw_ci,
            pal, x_offset, marker_sequence[1:n_models])
 
     legend(x='right', legend=models, pch=marker_sequence, col=pal)
+    return(list(dd=dfw_dd, ci=dfw_ci))
 }
 
 df <- read.csv('./model_components_18Feb.csv')
@@ -99,5 +130,5 @@ fig3a_data <- df[df[['site_code']] %in% c('NHA', 'CMA', 'SCA') &
                  c('model', 'site_code', 'dd', 'dd_se_neff')]
 fig3a_data <- droplevels(fig3a_data)
 pdf('ECoast_gradient_with_std_error.pdf')
-gradient_CI_plot(fig3a_data)
+data <- gradient_CI_plot(fig3a_data)
 dev.off()
