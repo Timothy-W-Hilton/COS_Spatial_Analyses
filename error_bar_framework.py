843322456b7ee57388564cb39d9e88305cb0e5cc
Timothy W. Hilton <thilton@ucmerced.edu>
2016-02-24 15:19:08 -0800

    retool error_bar_framework to make a csv containing permutations of Fplant, etc.
    - (1) generates all combinations of Fplant, Fsoil, Fanthro, Fbound.
    - (2) calculates total (i.e. summed) [COS] from all components of (1)
    - (3) calculates July-August mean drawdown from (2)
    - (4) writes (3) to csv file

--- error_bar_framework.py
+++ error_bar_framework.py
@@ -6,13 +6,28 @@ from datetime import datetime
 from stem_pytools import NERSC_data_paths as ndp
 from aqout_postprocess_spatial_paper import AqoutContainerSpatialPaper
 from stem_pytools import STEM_mapper
+import itertools
 
 t0 = datetime.now()
 
 runs = ndp.get_Spatial_Paper_runs()
-aqcs = {k: AqoutContainerSpatialPaper(v.aqout_path, key=k)
-        for k, v in runs.items()}
-fnames = []
+Fplant = ['SiB_calc', 'SiB_mech', 'casa_gfed_161',
+          'casa_gfed_C4pctLRU', 'canibis_161', 'canibis_C4pctLRU']
+Fsoil = ['Fsoil_Kettle', 'Fsoil_Hybrid5Feb']
+Fanthro = ['Anthro_Andrew',  'Anthro_Kettle']
+Fbounds = [None, 'climatological_bnd']
+
+# gather all permutations and remove None (None is placeholder for
+# constant boundaries, which has no explicit AQOUT file)
+all_combos = [c for c in itertools.product(Fplant, Fsoil, Fanthro, Fbounds)]
+all_combos = [[this_run for this_run in this_set if this_run is not None]
+              for this_set in all_combos]
+
+aqcs = {"-".join(this_set): AqoutContainerSpatialPaper(
+    aqout_paths=[runs[this_run].aqout_path for this_run in this_set],
+    key="-".join(this_set)) for this_set in all_combos}
+# aqcs = {k: v for k, v in aqcs_all.items()[0:2]}
+
 
 # for k, this_aqc in aqcs.items()[(1, -1)]:
 # aqcs = dict((k, aqcs_all[k]) for k in ['casa_gfed_161'])
@@ -22,7 +37,7 @@ for k, this_aqc in aqcs.items():
     print "processing {}".format(k)
     # this_aqc.parse(t0=None, t1=None, verbose=True)
     this_aqc.parse(t0=datetime(2008, 7, 8),
-                   t1=datetime(2008, 8, 31, 23, 59, 59),
+                   t1=datetime(2008, 8, 31, 0, 0, 0),
                    verbose=True)
     this_aqc.sum()
     # this_aqc.calc_stats()
@@ -32,7 +47,7 @@ for k, this_aqc in aqcs.items():
         '/project/projectdirs/m2319/Data/NOAA_95244993/')
 
 all = pd.concat([this_model.site_vals for this_model in aqcs.values()])
-all.to_csv('./model_components_18Feb.csv')
+all.to_csv('./model_components_24Feb.csv')
 print datetime.now() - t0
 
 # se_map = STEM_mapper.Mapper124x124(aqcs['canibis_161'].dd_se)
