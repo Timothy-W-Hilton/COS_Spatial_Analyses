33ac6e476234caa11707066f20a80e7b7d1eddd4
Timothy W. Hilton <thilton@ucmerced.edu>
2016-04-04 10:47:05 -0700

    implement panels in c3/c4 map plot

--- draw_c3c4LRU_map.py
+++ draw_c3c4LRU_map.py
@@ -1,18 +1,69 @@
 import matplotlib
 matplotlib.use('AGG')
 
+import matplotlib.pyplot as plt
 import netCDF4
 import os
 import os.path
 import numpy as np
 from mpl_toolkits.basemap import maskoceans
+import matplotlib.gridspec as gridspec
 
 from stem_pytools import na_map
 from stem_pytools.domain import STEM_Domain
 from timutils import midpt_norm
+import timutils.io
 
 
-def draw_map():
+def setup_panel_array(nrows=3, ncols=6, figsize=(6, 3)):
+    """create a matrix of axes for maps with individual color bars
+
+    create a figure containing a matrix of axes with nrows rows and
+    ncols columns, where each "panel" consists of a main axis using
+    90% of the horizontal space and a secondary colorbar axis
+    occupying 10% of the horizontal space.
+
+    ARGS
+    nrows (int): number of rows of axes
+    ncols (int): number of columns of axes
+    figsize ((float, float)): width and height of figure in inches
+
+    OUTPUTS
+    fig: matplotlib.figure.Figure object
+    ax: nrows by ncols numpy array of
+        matplotlib.axes._subplots.AxesSubplot objects
+    cbar_ax: nrows by ncols numpy array of
+        matplotlib.axes._subplots.AxesSubplot objects (for colorbars)
+    """
+    fig = plt.figure(figsize=figsize)
+    # two gridspecs - one for maps, one for colorbasr
+
+    gs = gridspec.GridSpec(
+        nrows,
+        ncols * 2,
+        width_ratios=[9, 1] * ncols)
+    # gs_maps = gs[range(0, ncols * 2, 2), range(nrows)]
+    # gs_maps = gs[range(1, ncols * 2, 2), range(nrows)]
+    # gs_maps.update(hspace=0.01, wspace=0.0, left=0.0, right=0.87)
+    # gs_cb.update(hspace=0.5, wspace=0.0, left=0.93, right=0.96)
+
+    # arrays to hold axis handles
+    ax = np.empty((nrows, ncols), dtype='object')
+    cbar_ax = np.empty((nrows, ncols), dtype='object')
+    for this_row in range(nrows):
+        for this_col in range(ncols):
+            ax[this_row, this_col] = plt.subplot(
+                gs[this_row, this_col * 2])
+            cbar_ax[this_row, this_col] = plt.subplot(
+                gs[this_row, (this_col * 2) + 1])
+
+    # make sure we got a unique axis for all maps, colorbars
+    assert np.unique(np.concatenate((ax, cbar_ax))).size == (nrows * ncols * 2)
+    fig.savefig(timutils.io.get_temp_filename(prefix='image01_'))
+    return(fig, ax, cbar_ax)
+
+
+def draw_map(map_axis=None, cb_axis=None):
     """parse the C3/C4 weighted average LRU and plot it over a map of
     North America.
 
@@ -34,16 +85,21 @@ def draw_map():
         vmin=1.12,
         vmax=1.84,
         midpoint=1.61)
-    lru_map = na_map.NAMapFigure(label_latlon=True,
-                                 cb_axis=True,
-                                 t_str='LRU from C4 veg pct')
+
+    lru_map = na_map.NAMapFigure(map_axis=map_axis,
+                                 label_latlon=True,
+                                 cb_axis=cb_axis,
+                                 t_str=None)
+    map_axis.figure.savefig(timutils.io.get_temp_filename(prefix='image02_'))
 
     cm = lru_map.map.pcolor(stem_lon, stem_lat,
                             maskoceans(stem_lon, stem_lat, LRU),
                             cmap=fcos_cmap,
                             latlon=True,
                             norm=fcos_norm)
-    cbar = lru_map.fig.colorbar(cm, cax=lru_map.ax_cmap)
+    map_axis.figure.savefig(timutils.io.get_temp_filename(prefix='image03_'))
+    cbar = map_axis.figure.colorbar(cm, cax=cb_axis, format='%0.2f')
+    map_axis.figure.savefig(timutils.io.get_temp_filename(prefix='image04_'))
     # ticks = np.concatenate([np.linspace(1.12, 1.84, 5), np.array([1.61])])
     # cbar = lru_map.fig.colorbar(cm, cax=lru_map.ax_cmap, ticks=ticks)
     # get rid of white lines in colorbar?
@@ -54,5 +110,9 @@ def draw_map():
 
 
 if __name__ == "__main__":
-    c3c4_map = draw_map()
-    c3c4_map.fig.savefig('c3c4_map.png')
+    plt.close('all')
+    fig, ax, cb_ax = setup_panel_array(nrows=1, ncols=2)
+    c3c4_map = draw_map(ax[0, 0], cb_ax[0, 0])
+
+    c3c4_map = draw_map(ax[0, 1], cb_ax[0, 1])
+    fig.savefig('c3c4_map.png')
