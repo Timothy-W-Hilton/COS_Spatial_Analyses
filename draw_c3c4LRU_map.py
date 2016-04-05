a995af46cef80de4c634ac39c9d46c8a8e9fb113
Timothy W. Hilton <thilton@ucmerced.edu>
2016-04-05 10:21:20 -0700

    factor out mapping common tasks in draw_c3c4LRU_map.py
    - normalization, labels, etc. now placed in draw_map_common

--- draw_c3c4LRU_map.py
+++ draw_c3c4LRU_map.py
@@ -7,12 +7,10 @@ import os
 import os.path
 import numpy as np
 from mpl_toolkits.basemap import maskoceans
-import matplotlib.gridspec as gridspec
 
 from stem_pytools import na_map
 from stem_pytools.domain import STEM_Domain
 from timutils import midpt_norm
-import timutils.io
 
 
 def setup_panel_array(nrows=3, ncols=6, figsize=(6, 3)):
@@ -50,92 +48,85 @@ def draw_c3c4_pct_map(map_axis=None, cb_axis=None,
     pct = nc.variables['C4pct'][...].squeeze()
     nc.close()
 
-    d = STEM_Domain()
-    stem_lon = d.get_lon()
-    stem_lat = d.get_lat()
-
-    pct_cmap, pct_norm = midpt_norm.get_discrete_midpt_cmap_norm(
-        vmin=0.0,
-        vmax=100.0,
-        midpoint=50.0,
-        this_cmap=plt.get_cmap('Blues'),
-        extend='neither')
-
-    pct_map = na_map.NAMapFigure(map_axis=map_axis,
-                                 label_latlon=(label_lat, label_lon),
-                                 lon_label_interval=30,
-                                 cb_axis=cb_axis,
-                                 t_str=None)
-
-    cm = pct_map.map.pcolor(stem_lon, stem_lat,
-                            maskoceans(stem_lon, stem_lat, pct),
-                            cmap=pct_cmap,
-                            latlon=True,
-                            norm=pct_norm)
-    cbar = map_axis.figure.colorbar(cm,
-                                    ax=map_axis,
-                                    format='%0.1f',
-                                    orientation='horizontal')
-    ticklabs = cbar.ax.get_xticklabels()
-    tickvals = np.array([float(x.get_text()) for x in ticklabs])
-    cbar.ax.set_xticklabels(tickvals, rotation=-45)
-    # place 'b' label in upper left
-    map_axis.text(-0.1, 1.0, 'b', transform=map_axis.transAxes)
-    return pct_map
+    draw_map_common(data=pct,
+                    map_axis=map_axis,
+                    cb_axis=cb_axis, cb_format='%0f',
+                    label_lat=label_lat, label_lon=label_lon,
+                    vmin=0, vmax=100, midpoint=50,
+                    cmap=plt.get_cmap('Blues'),
+                    panel_lab='b')
 
 
-def draw_map(map_axis=None, cb_axis=None, label_lat=False, label_lon=False):
-    """parse the C3/C4 weighted average LRU and plot it over a map of
-    North America.
+def draw_c3c4_LRU_map(map_axis=None, cb_axis=None,
+                      label_lat=False, label_lon=False):
 
-    returns: stem_pytools.na_map.NAMapFigure object containing the map
-    """
-
-    # nc = netCDF4.Dataset(os.path.join(os.environ['WORK'], 'Data',
-    #                                   'STEM', 'input', 'LRU_from_C4pct.nc'))
     nc = netCDF4.Dataset(os.path.join(os.environ['PROJ'],
                                       'Data',
                                       'C4_percentage',
                                       'LRU_from_C4pct.nc'))
     LRU = nc.variables['LRU'][...].squeeze()
     nc.close()
+
+    lru_map = draw_map_common(data=LRU,
+                              map_axis=map_axis,
+                              cb_axis=cb_axis, cb_format='%0.2f',
+                              label_lat=label_lat, label_lon=label_lon,
+                              vmin=1.12, vmax=1.84, midpoint=1.61,
+                              cmap=plt.get_cmap('PuOr'),
+                              panel_lab='a')
+    return lru_map
+
+
+def draw_map_common(data,
+                    map_axis=None,
+                    cb_axis=None, cb_format='%0.2f',
+                    label_lat=False, label_lon=False,
+                    vmin=None, vmax=None, midpoint=None,
+                    cmap=plt.get_cmap('Blues'),
+                    panel_lab='a'):
+    """
+    returns: stem_pytools.na_map.NAMapFigure object containing the map
+    """
+
     d = STEM_Domain()
     stem_lon = d.get_lon()
     stem_lat = d.get_lat()
-    fcos_cmap, fcos_norm = midpt_norm.get_discrete_midpt_cmap_norm(
-        vmin=1.12,
-        vmax=1.84,
-        midpoint=1.61,
-        extend='neither')
+    this_cmap, this_norm = midpt_norm.get_discrete_midpt_cmap_norm(
+        vmin=vmin,
+        vmax=vmax,
+        midpoint=midpoint,
+        extend='neither',
+        this_cmap=cmap)
 
-    lru_map = na_map.NAMapFigure(map_axis=map_axis,
+    map_obj = na_map.NAMapFigure(map_axis=map_axis,
                                  label_latlon=(label_lat, label_lon),
                                  lon_label_interval=30,
                                  cb_axis=cb_axis,
                                  t_str=None)
 
-    cm = lru_map.map.pcolor(stem_lon, stem_lat,
-                            maskoceans(stem_lon, stem_lat, LRU),
-                            cmap=fcos_cmap,
+    cm = map_obj.map.pcolor(stem_lon, stem_lat,
+                            maskoceans(stem_lon, stem_lat, data),
+                            cmap=this_cmap,
                             latlon=True,
-                            norm=fcos_norm)
+                            norm=this_norm)
+    print 'colorbar format: ' + cb_format
     cbar = map_axis.figure.colorbar(cm,
                                     ax=map_axis,
-                                    format='%0.2f',
+                                    format=cb_format,
                                     orientation='horizontal')
     ticklabs = cbar.ax.get_xticklabels()
     tickvals = np.array([float(x.get_text()) for x in ticklabs])
     cbar.ax.set_xticklabels(tickvals, rotation=-45)
-    # place 'b' label in upper left
-    map_axis.text(-0.2, 1.0, 'a', transform=map_axis.transAxes)
-    return lru_map
+    # place panel label in upper left
+    map_axis.text(-0.2, 1.0, panel_lab, transform=map_axis.transAxes)
+    return map_obj
 
 
 if __name__ == "__main__":
     plt.close('all')
     fig, ax = setup_panel_array(nrows=1, ncols=2)
-    c3c4_LRU_map = draw_map(ax[0],
-                            label_lon=True, label_lat=True)
+    c3c4_LRU_map = draw_c3c4_LRU_map(ax[0],
+                                     label_lon=True, label_lat=True)
     c3c4_pct_map = draw_c3c4_pct_map(ax[1],
                                      label_lon=True, label_lat=False)
     fig.tight_layout()
